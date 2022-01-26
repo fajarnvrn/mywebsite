24d7878daee737163008b104169e8cc7c339d3b1
fajarnvrn <fajar.armaidi@students.amikom.ac.id>
2022-01-27 06:05:27 +0700

    mywebsite

---
+++ myweb/views.py
@@ -0,0 +1,10 @@
+import imp
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render (request,'index.html')  
+
+def about(request):
+    return HttpResponse ('halo')
+  
